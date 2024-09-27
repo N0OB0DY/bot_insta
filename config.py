00b3be5979731b673102191e05bb346d493a9dcc
N0OB0DY <jhonywonk4@gmail.com>
2024-09-27 12:23:29 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+usuario = "*********"        # seu_usuario
+senha = "**********"         # sua_senha
+mensagem = "Ola, mundo!"     # sua_mensagem
+
+links = [
+
+    "ink_do_perfil_1",    # link_do_perfil_1
+    "link_do_perfil_2",   # link_do_perfil_2
+    "link_do_perfil_3"    # link_do_perfil_3
+    # mais links
+]
